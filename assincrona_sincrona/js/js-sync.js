1dd3f320d34863a8feccc38b57fcceadb0bf8b72
Vinicius Coutinho <mingal.rox@gmail.com>
2019-12-18 20:17:10 -0300

    adicinando

---
+++ assincrona_sincrona/js/js-sync.js
@@ -0,0 +1,7 @@
+var syncP = "This a example of program sync";
+
+for (let index = 0; index <= 10 ; index++) {
+    console.log(index);
+}
+
+console.log(`for sync ${syncP}`);
---
+++ imports/js-dominio.js
@@ -0,0 +1,1 @@
+console.log("Carregando js via arquivo externo mesmo dominio");
