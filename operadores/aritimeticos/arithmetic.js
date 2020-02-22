309c43d5734764f8e68b4aeaa46985b71e00a2b6
Vinicius Coutinho <mingal.rox@gmail.com>
2020-02-22 11:33:13 -0300

    add

---
+++ operadores/aritimeticos/arithmetic.js
@@ -0,0 +1,37 @@
+//sum
+// console.log("Sum         #################");
+// var sum = 15 + 15;
+
+// console.log(sum);
+// console.log("20" + "20");
+// var numStr = "27";
+
+// console.log(typeof numStr);
+// console.log(typeof +numStr);
+// console.log(true + false + true);
+// console.log("10 + null", 10 + null);
+// console.log("10 + true", 10 + true);
+// console.log("10 + undefined", 10 + undefined);
+// console.log("Possible problem with patern IEEE 754 0.2 + 0.1 = ", 0.2 + 0.1);
+// console.log("10.33 + 11.01 = ", 10.33 + 11.01);
+
+
+
+
+
+// //subtraction
+// console.log("Subtraction #################");
+// var sub = 500 - 300;
+// console.log("500 - 300 =", sub);
+// console.log("-10 -30 =", -10 -30);
+// console.log("-10 +30 =", -10 +30);
+// console.log("+10 = ", -10);
+// console.log("5 - true = ", 5 - true);
+// var strNum = "25";
+// console.log("typeof -(strNum = '56')",typeof -strNum);
+
+
+//multiplication
+console.log("Multiplication #################");
+
+
