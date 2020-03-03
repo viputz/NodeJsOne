55e6ff7b66cdd463ec70e731cbcc002aed19c8f5
Vinicius Coutinho <mingal.rox@gmail.com>
2020-03-02 22:33:14 -0300

    finish operators arithmetic

--- operadores/aritimeticos/arithmetic.js
+++ operadores/aritimeticos/arithmetic.js
@@ -31,7 +31,56 @@
 // console.log("typeof -(strNum = '56')",typeof -strNum);
 
 
-//multiplication
-console.log("Multiplication #################");
+// //multiplication 
+// console.log("Multiplication #################");
+// var mult = 5 * 20;
+// console.log("5 * 20 =", mult);
+// console.log("mutliplication with string number 10 * '2' =", 10 * "2");
+// console.log("precedence: 2 + 2 * 3 =", 2 + 2 * 3);
+// console.log("precedence with (): (2 + 2) * 3 =", (2 + 2) * 3);
+// console.log("5 * 0 =", 5 * 0);
+// console.log("0 * 5 =", 0 * 5);
+// console.log("3 * -5 =", 3 * -5);
+// console.log("-3 * -5 =", -3 * -5);
 
 
+//Division
+// console.log("Division #################");
+// var div = 50 / 2;
+// console.log("50/2 = ", div);
+// console.log("50 /0", 50 / 0);
+// console.log("0 /50", 0 / 50);
+
+//Module
+// console.log("Module ###################");
+// var mod = 10 % 2;
+// console.log("10 % 2", mod);
+// console.log("10 % 3", 10 % 3);
+
+
+
+// //increment
+// console.log("Incremento ###################");
+// var inc = 5;
+// inc++;
+// console.log("1 - inc 5 (++)", inc);
+// console.log("2 - inc++", inc++);
+// console.log("3 - inc", inc);
+// console.log("4 - ++inc", ++inc);
+// console.log("5 - inc", inc);
+
+// //Decrement
+// console.log("Decremento ###################");
+// var inc = 5;
+// inc--;
+// console.log("1 - inc 5 (--)", inc);
+// console.log("2 - inc++", inc--);
+// console.log("3 - inc", inc);
+// console.log("4 - --inc",--inc);
+// console.log("5 - inc", inc);
+
+
+// //exponentiation
+// console.log("exponentiation/potentiation");
+// var exp = 2 ** 3;
+// console.log("2 ** 3 = ", exp);
