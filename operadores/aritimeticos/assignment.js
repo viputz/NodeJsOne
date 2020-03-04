214e87668ab49217244c36ef0378192a98cf57b4
Vinicius Coutinho <mingal.rox@gmail.com>
2020-03-03 22:27:07 -0300

    assigments and equals

---
+++ operadores/aritimeticos/assignment.js
@@ -0,0 +1,48 @@
+// // operator '='
+// var number = 5;
+// var number2 = 2;
+// var name = "Vinicius";
+
+// number = number = number2;
+
+// console.log(number);
+
+//operator '<<=' left shift
+
+// console.log("operator '<<=' left shift ###########");
+// var num1 = 210;
+// console.log("1 - num1 on base10: ", num1);
+// console.log("2 - num1 on base2: ", num1.toString(2));
+
+// num1 <<= 5;
+
+// console.log("3 - num1 on base2: ", num1.toString(2));
+// console.log("4 - num1 on base10: ", num1);
+
+
+
+// //operator '>>=' rigth shift
+
+// console.log("operator '>>=' rigth shift ###########");
+// var num1 = 20;
+// console.log("1 - num1 on base10: ", num1);
+// console.log("2 - num1 on base2: ", num1.toString(2));
+
+// num1 >>= 5;
+
+// console.log("3 - num1 on base2: ", num1.toString(2));
+// console.log("4 - num1 on base10: ", num1);
+
+
+
+//operator '>>>=' rigth shift no sinal
+
+console.log("operator '>>>=' rigth shift ###########");
+var num1 = -350;
+console.log("1 - num1 on base10: ", num1);
+console.log("2 - num1 on base2: ", num1.toString(2));
+
+num1 >>>= 7;
+
+console.log("3 - num1 on base2: ", num1.toString(2));
+console.log("4 - num1 on base10: ", num1);
